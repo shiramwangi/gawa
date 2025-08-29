4dace2982f592ccb6a2732d13b3bc30a34e4ecbb
ismahaneyy <ismahanabdirizakk@gmail.com>
2025-08-30 00:32:05 +0300

    delete backend

--- backend/app/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from ..database import Base
-from .user import User
-from .meal import Meal
-from .order import Order
-from .order_item import OrderItem
-from .payment import Payment
-
-__all__ = ["Base", "User", "Meal", "Order", "OrderItem", "Payment"]
